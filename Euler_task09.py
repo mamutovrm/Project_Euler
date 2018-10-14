21ad0feded821b1b3d22678cc8bb1c72bbf6c9cb
mamutovrm1 <mamutovrm1@gmail.com>
2018-10-14 17:58:02 +0300

    -

--- Euler_task09.py
+++ Euler_task09.py
@@ -5,12 +5,13 @@ b = 4
 c = 5
 
 flag = False
-for i in range (a, 333):
-    for j in range (i+1, 1000-i):
+for i in range(a, 333):
+    for j in range(i+1, 1000-i):
         z = math.sqrt(i**2 + j**2)
         if z.is_integer():
             if i + j + z == 1000:
-                print("{}; {}; {}; ".format(i, j, int(z)) + " SUMM = 1000; composion = " + str(i*j*z))
+                print("{}; {}; {}; ".format(i, j, int(z)) +
+                      " SUMM = 1000; composion = " + str(i*j*z))
                 flag = True
         if flag:
             break
